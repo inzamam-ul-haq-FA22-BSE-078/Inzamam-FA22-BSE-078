1e455935d3154c02da186397c490ca686eaa3923
inzamam-ul-haq-FA22-BSE-078 <itx.inzi.078@gmail.com>
2025-03-03 08:39:03 +0500

    mobile app name

---
+++ mobile_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Center(child: Text('inzamam ul haq'),),);
+  }
+}
+
+
